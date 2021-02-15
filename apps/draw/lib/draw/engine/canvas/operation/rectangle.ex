3941455365e4e3b2c0f42cda59b89bebdc1b4ff9
Bartosz Kalinowski <bartosz@blockfi.com>
2021-02-15 21:43:35 +0100

    fix rectangles to use the apply_changes api

--- apps/draw/lib/draw/engine/canvas/operation/rectangle.ex
+++ apps/draw/lib/draw/engine/canvas/operation/rectangle.ex
@@ -11,40 +11,56 @@ defmodule Draw.Engine.Canvas.Operation.Rectangle do
   """
   alias Draw.Engine
   alias Draw.Engine.Canvas
+  alias Draw.Engine.Canvas.Changes
   alias Draw.Engine.Canvas.Operation
   alias Draw.Engine.Canvas.Operation.Rectangle
 
   @type t :: %Rectangle{}
   @type error :: :non_positive_width | :non_positive_height | :out_of_bounds | :missing_character
+  @typep character_type :: {:fill, Engine.ascii()} | {:outline, Engine.ascii()}
+  @type opts :: [character_type()]
 
-  defstruct [:point, :width, :height, fill_character: 32, outline_character: 32]
+  defstruct [:point, :width, :height, :fill, :outline]
 
   @doc """
-  Helper to create new rectangle
+  Helper to create new rectangle.
 
-  # Example
-  iex> Rectangle.new({1, 2}, 3, 4, "X", "Y")
+  It doesn't run validations as structs can be created manually
+  anyway so no point enforcing validations on the creation of
+  Rectangle operation.
+
+  # Examples
+  iex> Rectangle.new({1, 2}, 3, 4, [fill: "X", outline: "Y"])
   %Rectangle{
     point: {1, 2},
     width: 3,
     height: 4,
-    fill_character: 89,
-    outline_character: 88
+    fill: "X",
+    outline: "Y"
+  }
+
+  iex> Rectangle.new({1, 2}, 3, 4)
+  %Rectangle{
+    point: {1, 2},
+    width: 3,
+    height: 4,
+    fill: nil,
+    outline: nil
   }
   """
-  @spec new(Engine.point(), integer(), integer(), String.t(), String.t()) :: Rectangle.t()
-  def new(point, width, height, <<outline_character>> = _outline, <<fill_character>> = _fill) do
+  @spec new(Engine.point(), integer(), integer(), opts()) :: Rectangle.t()
+  def new(point, width, height, opts \\ []) do
     %Rectangle{
       point: point,
       width: width,
       height: height,
-      fill_character: fill_character,
-      outline_character: outline_character
+      fill: Keyword.get(opts, :fill),
+      outline: Keyword.get(opts, :outline)
     }
   end
 
   @doc """
-  [INTERNAL] Validate if the rectangle can be drawn on the canvas.
+  Validates if the rectangle can be drawn on the canvas.
   """
   @spec validate(Canvas.t(), Rectangle.t()) :: :ok | {:error, error()}
   def validate(%Canvas{} = canvas, %Rectangle{point: {x, y} = point} = rectangle) do
@@ -61,7 +77,7 @@ defmodule Draw.Engine.Canvas.Operation.Rectangle do
       Canvas.at(canvas, {x + rectangle.width - 1, y + rectangle.height - 1}) == nil ->
         {:error, :out_of_bounds}
 
-      rectangle.fill_character == 32 && rectangle.outline_character == 32 ->
+      is_nil(rectangle.fill) && is_nil(rectangle.outline) ->
         {:error, :missing_character}
 
       true ->
@@ -70,38 +86,54 @@ defmodule Draw.Engine.Canvas.Operation.Rectangle do
   end
 
   @doc """
-  [INTERNAL] Draw the rectangle on the canvas. Should only be used after validation
+  Generates changes to draw the rectangle on the canvas.
+  Should be used after validation.
   """
-  @spec draw(Canvas.t(), Rectangle.t()) :: {:ok, Canvas.t()} | {:error, error()}
-  def draw(%Canvas{} = canvas, %Rectangle{} = rectangle) do
-    fields =
-      rectangle
-      |> generate_changes()
-      |> Enum.reduce(canvas.fields, fn {point, character}, fields ->
-        %{fields | point => character}
-      end)
+  @spec draw(Rectangle.t()) :: Changes.t()
+  def draw(%Rectangle{fill: nil, outline: <<outline>>, point: {x1, y1}} = rectangle) do
+    x2 = x1 + rectangle.width - 1
+    y2 = y1 + rectangle.height - 1
 
-    %{canvas | fields: fields}
+    fields =
+      for i <- x1..x2, j <- y1..y2, i == x1 || i == x2 || j == y1 || j == y2, into: %{} do
+        {{i, j}, outline}
+      end
+
+    %Changes{fields: fields}
   end
 
-  @spec generate_changes(Rectangle.t()) :: [{{integer(), integer()}, integer()}]
-  defp generate_changes(%Rectangle{point: {x, y}} = rectangle) do
-    width = rectangle.width
-    height = rectangle.height
+  def draw(%Rectangle{fill: <<fill>>, outline: nil, point: {x1, y1}} = rectangle) do
+    x2 = x1 + rectangle.width - 1
+    y2 = y1 + rectangle.height - 1
 
-    for i <- x..(x + width - 1), j <- y..(y + height - 1) do
-      if i == x || j == y || i == x + width - 1 || j == y + height - 1 do
-        {{i, j}, rectangle.outline_character}
-      else
-        {{i, j}, rectangle.fill_character}
+    fields =
+      for i <- x1..x2, j <- y1..y2, into: %{} do
+        {{i, j}, fill}
       end
-    end
+
+    %Changes{fields: fields}
+  end
+
+  def draw(%Rectangle{fill: <<fill>>, outline: <<outline>>, point: {x1, y1}} = rectangle) do
+    x2 = x1 + rectangle.width - 1
+    y2 = y1 + rectangle.height - 1
+
+    fields =
+      for i <- x1..x2, j <- y1..y2, into: %{} do
+        if i == x1 || i == x2 || j == y1 || j == y2 do
+          {{i, j}, outline}
+        else
+          {{i, j}, fill}
+        end
+      end
+
+    %Changes{fields: fields}
   end
 
   defimpl Operation do
     def process(%Rectangle{} = operation, %Canvas{} = canvas) do
       case Rectangle.validate(canvas, operation) do
-        :ok -> {:ok, Rectangle.draw(canvas, operation)}
+        :ok -> {:ok, Rectangle.draw(operation)}
         {:error, error} -> {:error, error}
       end
     end
