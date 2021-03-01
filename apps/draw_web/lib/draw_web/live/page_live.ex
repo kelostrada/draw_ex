8bf9858c3c976487d4f36455f9ec8557faa984ce
Bartosz Kalinowski <kelostrada@gmail.com>
2021-03-01 12:29:36 +0100

    Fix page_live to redirect when creating new canvas

--- apps/draw_web/lib/draw_web/live/page_live.ex
+++ apps/draw_web/lib/draw_web/live/page_live.ex
@@ -7,11 +7,25 @@ defmodule DrawWeb.PageLive do
 
   @impl true
   def mount(params, _session, socket) do
-    canvas_id = Map.get(params, "canvas_id")
-    PubSub.subscribe(Draw.PubSub, "canvas:#{canvas_id}")
+    case Map.get(params, "canvas_id") do
+      nil ->
+        socket = fetch_canvas(socket)
+        {:ok, push_redirect(socket, to: "/?canvas_id=#{socket.assigns.canvas_id}", replace: true)}
+
+      canvas_id ->
+        {:ok, fetch_canvas(socket, canvas_id)}
+    end
+  end
+
+  defp fetch_canvas(socket, canvas_id \\ nil) do
     {:ok, canvas_id} = Draw.init_canvas(canvas_id)
+    PubSub.subscribe(Draw.PubSub, "canvas:#{canvas_id}")
+
     canvas = Draw.Server.get_canvas(canvas_id)
-    {:ok, socket |> assign(canvas: canvas) |> assign(canvas_id: canvas_id)}
+
+    socket
+    |> assign(canvas: canvas)
+    |> assign(canvas_id: canvas_id)
   end
 
   @impl true
