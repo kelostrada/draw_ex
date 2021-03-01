bdc425ab1472135126bb12c104e854f2c30a06ff
Bartosz Kalinowski <kelostrada@gmail.com>
2021-03-01 15:16:12 +0100

    fix tests and warnings

--- apps/draw_web/test/draw_web/live/page_live_test.exs
+++ apps/draw_web/test/draw_web/live/page_live_test.exs
@@ -4,7 +4,8 @@ defmodule DrawWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
+    assert {:error, {:live_redirect, %{to: "/?canvas_id=" <> canvas_id}}} = live(conn, "/")
+    {:ok, page_live, disconnected_html} = live(conn, "/?canvas_id=#{canvas_id}")
     assert disconnected_html =~ "Canvas"
     assert render(page_live) =~ "Canvas"
   end
